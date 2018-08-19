18ed8083c6f1063779d8cbfff5cbc976520b1e11
abhishek-toolsee <abhishek.toolsee@umail.uom.ac.mu>
2018-08-19 11:29:51 +0400

    GPS running on a service

--- app/src/main/java/com/example/libra/as_gps_tracking/GPSTracker.java
+++ app/src/main/java/com/example/libra/as_gps_tracking/GPSTracker.java
@@ -1,6 +1,7 @@
 package com.example.libra.as_gps_tracking;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -10,86 +11,76 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 
-public class GPSTracker extends Service implements LocationListener {
+/**
+ * Class for running GPS in background
+ */
+public class GPSTracker extends Service {
 
-    private final int iUPDATE_LOCATION_TIME = 5000;
-    private final int iUPDATE_LOCATION_DISTANCE = 1;
-    private final String sMSG_GPS_NOT_ENABLED = "GPS not enabled.";
-    private final String sMSG_NETWORK_NOT_ENABLED = "Network not enabled.";
-    private final Context _objContext;
+    //region Private declarations
 
-    private boolean _isGPSEnabled = false;
-    private boolean _isNetworkEnabled = false;
-    private boolean _isLocationAvailable = false;
+    private static final int iLOCATION_INTERVAL = 5000;
+    private static final int iLOCATION_DISTANCE = 5000;
+    private LocationListener _objLocationListener;
+    private LocationManager _objLocationManager;
 
-    private Location _objLocation;
-    protected LocationManager _objLocationManager;
+    //endregion
 
-    public GPSTracker(Context objContext) {
-        this._objContext = objContext;
-    }
-
-    public Location getLocation() {
-
-        _objLocation = null;
-        _objLocationManager = (LocationManager) _objContext.getSystemService(LOCATION_SERVICE);
-        _isGPSEnabled = _objLocationManager.isProviderEnabled(_objLocationManager.GPS_PROVIDER);
-        _isNetworkEnabled = _objLocationManager.isProviderEnabled(_objLocationManager.NETWORK_PROVIDER);
-
-        try {
-            if (ContextCompat.checkSelfPermission(_objContext, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                    || ContextCompat.checkSelfPermission(_objContext, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                if (_isGPSEnabled){
-                    if (_objLocation == null) {
-                        _objLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, iUPDATE_LOCATION_TIME, iUPDATE_LOCATION_DISTANCE, this);
-                        if (_objLocationManager != null) {
-                            _objLocation = _objLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                        }
-                    }
-                }
-            }
-
-            /* if location is not found from GPS then it will be found from network */
-            if (_objLocation == null) {
-                if (_isNetworkEnabled){
-                    _objLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, iUPDATE_LOCATION_TIME, iUPDATE_LOCATION_DISTANCE, this);
-                    if (_objLocationManager != null) {
-                        _objLocation = _objLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                    }
-                }
-            }
-        } finally {
-
-        }
-        return _objLocation;
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
+    //region Public methods
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onCreate() {
+        _objLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Intent objIntent = new Intent(getString(R.string.sIntentFilterLocationUpdate));
+                objIntent.putExtra(getString(R.string.sIntentKeyLongitude), location.getLongitude());
+                objIntent.putExtra(getString(R.string.sIntentKeyLatitude), location.getLatitude());
+                sendBroadcast(objIntent);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent objIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                objIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(objIntent);
+            }
+        };
+
+        _objLocationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+
+        _objLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, iLOCATION_INTERVAL, iLOCATION_DISTANCE, _objLocationListener);
+    }
+
+    /**
+     * Destroy listener when service is destroyed to avoid memory leaks.
+     */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(_objLocationManager != null)
+            _objLocationManager.removeUpdates(_objLocationListener);
+    }
+
+    //endregion
 }
--- app/src/main/java/com/example/libra/as_gps_tracking/MainActivity.java
+++ app/src/main/java/com/example/libra/as_gps_tracking/MainActivity.java
@@ -1,34 +1,54 @@
 package com.example.libra.as_gps_tracking;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+import android.os.Build;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-
-import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
     //region Private declarations
 
-    private File _objFileInfo;
-    private LocationManager _objLocationManager;
-    private LocationListener _objLocationListener;
     private TextView _txtvLongitude;
     private TextView _txtvLatitude;
     private TextView _txtvInfo;
-    private Location _objLocation;
+    private Button _btnStartService;
+    private Button _btnStopService;
+    private BroadcastReceiver _objBroadcastReceiver;
 
-    private Location _objStartLocation = null;
-    private Location _objEndLocation = null;
+    //endregion
+
+    //region Public methods
+
+    /**
+     * Handles user's response when permissions are needed.
+     * @param requestCode   Request code to identify the requested permission.
+     * @param permissions   Permissions needed.
+     * @param grantResults  Results of the permission (Granted or Not Granted).
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 100){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                enableButtons();
+            }
+            else{
+                checkRuntimePermissions();
+            }
+        }
+    }
 
     //endregion
 
@@ -39,82 +59,87 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        _objFileInfo = new File(getString(R.string.sFileInfo));
-
         _txtvLongitude = (TextView) findViewById(R.id.txtvLongitude);
         _txtvLatitude = (TextView) findViewById(R.id.txtvLatitude);
         _txtvInfo = (TextView) findViewById(R.id.txtvInfo);
+        _btnStartService = (Button) findViewById(R.id.btnStartService);
+        _btnStopService = (Button) findViewById(R.id.btnStopService);
 
-        _objLocationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
-        _objLocationListener = new LocationListener() {
-
-            @Override
-            public void onLocationChanged(Location location) {
-                try {
-                    if (location == null)
-                        return;
-
-                    Log.d("Location: ", location.toString());
-                    _txtvLongitude.setText(String.format("%s%f", getString(R.string.sLongitude), location.getLongitude()));
-                    _txtvLatitude.setText(String.format("%s%f", getString(R.string.sLatitude), location.getLatitude()));
-
-                    _objEndLocation = location;
-                    float dist = _objStartLocation.distanceTo(_objEndLocation);
-                    _objStartLocation = _objEndLocation;
-
-                    _txtvInfo.append(String.format("%n%s%.2f m", getString(R.string.sDistance), dist));
-                    _txtvInfo.append(String.format("%n%s%.2f m/s", getString(R.string.sSpeed), (dist / 10)));
-                    _txtvInfo.append("\n");
-
-                } catch (Exception ex) {
-
-                }
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
-        };
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        } else {
-            _objLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0, _objLocationListener);
-            _objStartLocation = _objLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            _objStartLocation = _objLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            _objLocation = _objLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        }
+        if(!checkRuntimePermissions())
+            enableButtons();
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                    == PackageManager.PERMISSION_GRANTED) {
-                _objLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0, _objLocationListener);
-            }
+    /**
+     * Register the broadcaster correctly.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(_objBroadcastReceiver == null){
+            _objBroadcastReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    _txtvLongitude.setText(String.format("%s%f", getString(R.string.sTextviewLongitude),
+                            intent.getExtras().get(getString(R.string.sIntentKeyLongitude))));
+
+                    _txtvLatitude.setText(String.format("%s%f", getString(R.string.sTextviewLatitude),
+                            intent.getExtras().get(getString(R.string.sIntentKeyLatitude))));
+                }
+            };
         }
+
+        registerReceiver(_objBroadcastReceiver, new IntentFilter(getString(R.string.sIntentFilterLocationUpdate)));
+    }
+
+    /**
+     * Unregister broadcaster
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(_objBroadcastReceiver != null)
+            unregisterReceiver(_objBroadcastReceiver);
+    }
+
+    //endregion
+
+    //region Private methods
+
+    /**
+     * Check the permissions during runtime when needed.
+     * Check is done for SDK above 23.
+     * @return  true if granted, false if not granted
+     */
+    private boolean checkRuntimePermissions() {
+        if(Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Enables the buttons if permissions are granted.
+     */
+    private void enableButtons() {
+        _btnStartService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent objIntent = new Intent(getApplicationContext(), GPSTracker.class);
+                startService(objIntent);
+            }
+        });
+
+        _btnStopService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent objIntent = new Intent(getApplicationContext(), GPSTracker.class);
+                stopService(objIntent);
+            }
+        });
     }
 
     //endregion
